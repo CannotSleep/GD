37ea48995b6807668e977c12473037115eb62d1e
Zyt <zyt_dsg@163.com>
2018-11-27 17:31:26 +0800

    data

---
+++ Temp.Data/RepositoryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Temp.Core.Data;
+using Temp.Core.Dependency;
+
+namespace Temp.Data
+{
+    /// <summary>
+    /// Repository
+    /// </summary>
+    public class RepositoryFactory
+    {
+        static public IRepository<T> Create<T>() where T : BaseEntity
+        {
+            return IocObjectManager.GetInstance().Resolve<IRepository<T>>();
+        }
+    }
+}
---
+++ Temp.Service/Common/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Temp.Core.Data;
+using Temp.Core.Dependency;
+
+namespace Temp.Service.Common
+{
+    public interface IService<T> : IDependency where T : BaseEntity
+    {
+
+        IRepository<T> UseRepository
+        {
+            get;
+        }
+
+
+
+    }
+}
